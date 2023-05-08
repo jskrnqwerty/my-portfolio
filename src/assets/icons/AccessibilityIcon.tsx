8f0adf11758e1227224ed256629eb439c242b9c9
jskrnqwerty <kainth.jaskaran@gmail.com>
2023-05-08 00:39:41 -0400

    skills added

--- src/assets/icons/AccessibilityIcon.tsx
+++ src/assets/icons/AccessibilityIcon.tsx
@@ -1,7 +1,40 @@
 import { IconType } from "../../types/types";
 
-const AccesibilityIcon = ({ fillColor, bgColor }: IconType) => {
-  return <></>;
+const AccesibilityIcon = ({ strokeColor }: IconType) => {
+  return (
+    <svg
+      // width="800px"
+      // height="800px"
+      viewBox="0 0 24 24"
+      xmlns="http://www.w3.org/2000/svg"
+      aria-labelledby="accessibilityHumanIconTitle"
+      stroke="#000000"
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      // fill="none"
+      color="#000000"
+      className="accessibility"
+      aria-label="Accessibility icon"
+    >
+      {" "}
+      {/* <title id="accessibilityHumanIconTitle">
+        Accessibility features
+      </title>{" "} */}
+      <circle
+        className="stroke-accent"
+        stroke={strokeColor}
+        cx="12"
+        cy="3"
+        r="1"
+      />
+      <path
+        className="stroke-accent"
+        stroke={strokeColor}
+        d="M16 21L12 13M12 13V7M12 13L8 21M12 7L18 9M12 7L6 9"
+      />{" "}
+    </svg>
+  );
 };
 
 export default AccesibilityIcon;
--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -1,24 +1,34 @@
+import AccesibilityIcon from "../assets/icons/AccessibilityIcon";
+import AgileScrumIcon from "../assets/icons/AgileScrumIcon";
 import ApiIcon from "../assets/icons/ApiIcon";
 import BigCommerceIcon from "../assets/icons/BigCommerceIcon";
 import BootstrapIcon from "../assets/icons/BootstrapIcon";
 import CreateReactAppIcon from "../assets/icons/CreateReactAppIcon";
+import CSSFlexBoxIcon from "../assets/icons/CssFlexboxIcon";
+import CSSGridIcon from "../assets/icons/CssGridIcon";
 import CssIcon from "../assets/icons/CssIcon";
+import DomIcon from "../assets/icons/DomIcon";
 import FigmaIcon from "../assets/icons/FIgmaIcon";
 import GitHubIcon from "../assets/icons/GitHubIcon";
 import GitHubPagesIcon from "../assets/icons/GitHubPagesIcon";
 import GitIcon from "../assets/icons/GitIcon";
 import HtmlIcon from "../assets/icons/HtmlIcon";
 import JavascriptIcon from "../assets/icons/JavascriptIcon";
+import MarkdownIcon from "../assets/icons/MarkdownIcon";
 import MuiIcon from "../assets/icons/MuiIcon";
 import NetlifyIcon from "../assets/icons/NetlifyIcon";
 import NpmIcon from "../assets/icons/NpmIcon";
+import PatternIcon from "../assets/icons/PatternIcon";
 import PhotoshopIcon from "../assets/icons/PhotoshopIcon";
 import PythonIcon from "../assets/icons/PythonIcon";
 import ReactIcon from "../assets/icons/ReactIcon";
 import ReactRouterIcon from "../assets/icons/ReactRouterIcon";
 import ReduxIcon from "../assets/icons/ReduxIcon";
+import ResponsiveDesignIcon from "../assets/icons/ResponsiveDesignIcon";
 import SassIcon from "../assets/icons/SassIcon";
+import SeoIcon from "../assets/icons/SeoIcon";
 import SquarespaceIcon from "../assets/icons/SquarespaceIcon";
+import ThinkingIcon from "../assets/icons/ThinkingIcon";
 import TypescriptIcon from "../assets/icons/TypescriptIcon";
 import ViteIcon from "../assets/icons/ViteIcon";
 import { iconColors, bgColors } from "../types/types";
@@ -134,14 +144,28 @@ const Skills = () => {
     { name: "Squarespace", icon: <SquarespaceIcon fillColor={accentColor} /> },
     { name: "BigCommerce", icon: <BigCommerceIcon fillColor={accentColor} /> },
     { name: "Python", icon: <PythonIcon fillColor={accentColor} /> },
-    { name: "Responsive Design", icon: "Logo" },
-    { name: "Accessibility", icon: "Logo" },
-    { name: "Agile • Scrum", icon: "Logo" },
-    { name: "SEO", icon: "Logo" },
-    { name: "Markdown", icon: "Logo" },
-    // { name: "DOM", icon: "Logo" },
-    // { name: "CSS Flexbox", icon: "Logo" },
-    // { name: "CSS Grid", icon: "Logo" },
+    {
+      name: "Responsive Design",
+      icon: <ResponsiveDesignIcon fillColor={accentColor} />,
+    },
+    {
+      name: "Accessibility",
+      icon: <AccesibilityIcon strokeColor={accentColor} />,
+    },
+    { name: "Agile • Scrum", icon: <AgileScrumIcon fillColor={accentColor} /> },
+    { name: "SEO", icon: <SeoIcon fillColor={accentColor} /> },
+    { name: "Markdown", icon: <MarkdownIcon fillColor={accentColor} /> },
+    { name: "DOM", icon: <DomIcon fillColor={accentColor} /> },
+    { name: "CSS Flexbox", icon: <CSSFlexBoxIcon strokeColor={accentColor} /> },
+    { name: "CSS Grid", icon: <CSSGridIcon fillColor={accentColor} /> },
+    {
+      name: "Pattern recognition",
+      icon: <PatternIcon fillColor={accentColor} />,
+    },
+    {
+      name: "Critical Thinking",
+      icon: <ThinkingIcon strokeColor={accentColor} />,
+    },
   ];
   return (
     <>
